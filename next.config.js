e53788dc847b96e663c27c86f9af2a2a7df74ce8
gardenialima1-a11y <gardenialima1@gmail.com>
2026-08-26 09:35:58 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse", "mammoth", "@react-pdf/renderer"],
+    outputFileTracingIncludes: {
+      "/api/candidatos/[id]/pdf": ["./node_modules/pdfkit/**/*", "./node_modules/@react-pdf/**/*"],
+      "/api/vagas/[id]/pdf": ["./node_modules/pdfkit/**/*", "./node_modules/@react-pdf/**/*"],
+    },
   },
 };
 
